c7e748aad7f1b012ea1ab592289765d4642bd798
Hazzard1912 <jhonnier.hernandez@correounivalle.edu.co>
2023-07-27 19:21:25 -0500

    Added classes to show Inheritance

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
 
+            Radio myRadio = new Radio(false, "Sony");
+
+            myRadio.SwitchOn();
+
+            myRadio.ListenRadio();
+
+            TV myTV = new TV(false, "Samsung");
+
+            myTV.SwitchOn();
+
+            myTV.WatchTV();
+
         }
     }
 }
